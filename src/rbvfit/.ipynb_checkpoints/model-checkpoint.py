57d36e0797197310b459a8816b8e8b94007ff212
rongmon <rongmon.bordoloi@gmail.com>
2019-07-12 11:53:18 -0400

    Revamp to include multiple transitions

--- src/rbvfit/.ipynb_checkpoints/model-checkpoint.py
+++ src/rbvfit/.ipynb_checkpoints/model-checkpoint.py
@@ -17,9 +17,9 @@ def map_theta2list(theta,ncl=1,nt=1):
     
     # First break theta into individual N,b,v values for all entries
     No_of_clumps=int(len(theta)/3)
-    N=theta[0:No_of_clumps]
-    b=theta[No_of_clumps:No_of_clumps+No_of_clumps]
-    vel=theta[2*No_of_clumps:3*No_of_clumps]
+	N=theta[0:No_of_clumps]
+	b=theta[No_of_clumps:No_of_clumps+No_of_clumps]
+	vel=theta[2*No_of_clumps:3*No_of_clumps]
     
     N_prime=[]
     b_prime=[]
@@ -27,15 +27,13 @@ def map_theta2list(theta,ncl=1,nt=1):
     # Now do conversion individually
     
     for i in range(0,nt):  
-        N_prime[ncl*i:ncl*i+ncl]=N[0:ncl] 
-        b_prime[ncl*i:ncl*i+ncl]=b[0:ncl]
-        v_prime[ncl*i:ncl*i+ncl]=vel[0:ncl]
-
-    # If there are Nuissance parameters add them
-    if len(theta) > ncl*nt:
-        N_prime[ncl*nt:] =N[ncl:] 
-        b_prime[ncl*nt:] =b[ncl:] 
-        v_prime[ncl*nt:] =vel[ncl:] 
+        N_prime[cl*i:cl*i+cl]=N[0:ncl] 
+        b_prime[cl*i:cl*i+cl]=b[0:ncl]
+        v_prime[cl*i:cl*i+cl]=vel[0:ncl]
+    
+    N_prime[ncl*nt:] =N[ncl:] 
+    b_prime[ncl*nt:] =b[ncl:] 
+    v_prime[ncl*nt:] =vel[ncl:] 
     
     theta_prime=np.concatenate((np.array(N_prime),np.array(b_prime),np.array(v_prime)))
     return theta_prime
@@ -71,7 +69,7 @@ class create_voigt(object):
 
     """
 
-    def __init__(self, zabs,lambda_rest,nclump,ntransition=1, FWHM = '6.5', grating='G130M',life_position='1'):
+    def __init__(self, zabs,lambda_rest,nclump,ntransition, FWHM = '6.5', grating='G130M',life_position='1'):
         # Setting up model paramters
         self.FWHM = FWHM
         self.grating = grating
@@ -79,22 +77,15 @@ class create_voigt(object):
         self.nclump=nclump
         self.ntransition = ntransition
         #Creating a redshift list that is equal to the length of all transitions and nuissance parameters 
-        zlist=np.array(zabs)
-        # If there are nuissance paramters
-        if len(zlist) > 1:
-            zlist=np.append(np.repeat(zlist[0],nclump*ntransition),zlist[1:])
-        else:
-            zlist=np.repeat(zlist[0],nclump*ntransition)
+        zlst=np.array(zabs)
+        zlist=np.append(np.repeat(zlist[0],nclump*ntransition),zlist[1:])
         self.zabs = zlist
-
         # Now create a list of rest wavelengths which could be used to create a line object
         lambda_rest=np.array(lambda_rest)
         lam_restlist=[]
         for i in range(0,ntransition):
-            lam_restlist=np.append(lam_restlist,np.repeat(lambda_rest[i],nclump))
-            # If there are nuissance paramters
-            if len(zlist) > 1:
-                lam_restlist=np.append(lam_restlist,lambda_rest[ntransition:])
+            lam_restlist=np.append(lam_restlist,np.repeat(lam_rest[i],nclump))
+            lam_restlist=np.append(lam_restlist,lam_rest[ntransition:])
         self.lambda_rest = lam_restlist
   
         self.compile_model()
